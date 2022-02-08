30d172761d8694064097e1e33dcc890c8bbf4f65
Annie Huang <annie.hn@gmail.com>
2022-02-08 22:17:39 +1100

    Display the todo list properly on the page.

--- rtk-todos/src/App.tsx
+++ rtk-todos/src/App.tsx
@@ -4,7 +4,23 @@ import { todoApi } from "./store";
 
 function TodoApp() {
   const { data: todos } = todoApi.useGetAllQuery();
-  return <div className="App">{JSON.stringify(todos)}</div>;
+
+  // return <div className="App">{JSON.stringify(todos)}</div>;
+  return (
+    <div className="App">
+      <div className="todos">
+        {todos?.map((todo) => (
+          <React.Fragment key={todo.id}>
+            <div>
+              <input type="checkbox" checked={todo.done} onChange={() => {}} />
+              <span>{todo.text}</span>
+            </div>
+            <button onClick={() => {}}>Delete</button>
+          </React.Fragment>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 function App() {
