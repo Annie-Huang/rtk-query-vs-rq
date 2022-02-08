7864245fe04376f33036423febe9388a6eebda36
Annie Huang <annie.hn@gmail.com>
2022-02-08 21:42:00 +1100

    Create the first endpoint of "getAll" to get all the todos

--- rtk-todos/src/store.ts
+++ rtk-todos/src/store.ts
@@ -9,7 +9,15 @@ export interface Todo {
 
 export const todoApi = createApi({
   reducerPath: "todoApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/" }),
   // invalidatable tags. Basically define the kind of entity types without your system.
   tagTypes: ["Todos"],
+  endpoints: (build) => ({
+    getAll: builder.query<Todo[], void>({
+      query: () => `todos`,
+      // I don't understand where is the id: "LIST" coming from as it's not in
+      // C:\react\rtk-query-vs-rq\server\src\modules\todos\todo.controller.ts
+      providesTags: [{ type: "Todos", id: "LIST" }],
+    }),
+  }),
 });
