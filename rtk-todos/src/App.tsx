90ac6e31cdbc304233050381ea1838c788e74a78
Annie Huang <annie.hn@gmail.com>
2022-02-08 21:46:55 +1100

    Use a <ApiProvider> to wrap <TodoApp>

--- rtk-todos/src/App.tsx
+++ rtk-todos/src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { todoApi } from "./store";
 
-function App() {
+function TodoApp() {
   return <div className="App"></div>;
 }
 
+function App() {
+  return (
+    <ApiProvider api={todoApi}>
+      <TodoApp />
+    </ApiProvider>
+  );
+}
 export default App;
